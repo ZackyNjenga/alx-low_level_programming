a30804da5628fceb7511c1a41d15962775bf5330
ZackyNjenga <zacharynjenga381@gmail.com>
2023-03-17 18:21:27 +0300

    m

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Entry point 
+*
+*Return: Walways 0 S(Success)
+*/
+int main(void)
+{
+     printf("With proper grammar, but the outcome is a piece of art, \n");
+     return (0);
+}
