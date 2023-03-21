613ec95e08d69fb8fc5c94ec70346b7443e9a5e1
ZackyNjenga <zacharynjenga381@gmail.com>
2023-03-21 16:14:12 +0300

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 
 /**
  * main - Check description
- * Description: It prints the word Holberton, followed by a new line.
+ * Description: It prints the word _putchar, followed by a new line.
  * Return: 0.
  */
 int main(void)
@@ -14,6 +14,5 @@ int main(void)
 	for (i = 0; i < 9; i++)
 		_putchar(word[i]);
 	_putchar('\n');
-
 	return (0);
 }
