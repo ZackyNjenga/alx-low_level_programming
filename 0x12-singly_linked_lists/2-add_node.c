1b110eabd0d60a56b187a179993da6223495bb7e
ZackyNjenga <zacharynjenga381@gmail.com>
2023-04-27 15:32:42 +0300

    Correction for question 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <string.h>
-#include <stdlib.h>
 
 /**
  * add_node fuction adds a node to the head of linked list
@@ -10,21 +9,19 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	unsigned int len = 0;
+	list_t *temp;
+	int length = 0;
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
 
 	while (str[length])
-	       len++;
-	       
-	new = malloc(sizeof(list_t));
-	if (!new)
-	   return(NULL);
-	
-	new -> str = strdup(str);
-	new ->len = len;
-	new -> next = (*head);
-	(*head) = new;
-	
-	
-	return(*head);
+		length++;
+
+	temp->len = length;
+	temp->str = strdup(str);
+	temp->next = *head;
+	*head = temp;
+	return (temp);
 }
